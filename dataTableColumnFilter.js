b3330f5cc98efbdfc5e3184c29c4f31eb2e85b52
Konstantin Kaluzhnikov <EvilKraft@users.noreply.github.com>
2017-12-15 17:51:09 +0400

    Add files via upload

---
+++ dataTableColumnFilter.js
@@ -0,0 +1,31 @@
+function dataTableColumnFilter( api ) {
+    api.columns('.select-filter').every( function () {
+        var column = this;
+        var title  = column.header().innerText;
+
+        var select = $('<select class="form-control" style="width: 100%; padding: 0; height: 20px;"><option value="">'+title+'</option></select>')
+            .appendTo( $(column.header()).empty() )
+            .on( 'change', function () {
+                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                column.search( val ? '^'+val+'$' : '', true, false ).draw();
+            }).on( 'click', function( event ) {
+                event.stopPropagation();
+            });
+
+        column.data().unique().sort().each( function ( d, j ) {
+            if(d){
+                select.append( '<option value="'+d+'">'+d+'</option>' )
+            }
+        });
+
+    });
+}
+
+
+$(document).on( 'init.dt', function ( e, settings ) {
+    var api = new $.fn.dataTable.Api( settings );
+
+    if ( api.init().dataTableColumnFilter || $.fn.dataTable.defaults.dataTableColumnFilter ){
+        new dataTableColumnFilter( api );
+    }
+} );
