1d7b19d65de828d67648d24f30afafef29a7bf20
Konstantin Kaluzhnikov <evilkraft@gmail.com>
2021-12-24 12:54:44 +0400

    Update dataTableColumnFilter.js

--- dataTableColumnFilter.js
+++ dataTableColumnFilter.js
@@ -1,12 +1,12 @@
 function dataTableColumnFilter( api ) {
     api.columns('.select-filter').every( function () {
-        var column = this;
-        var title  = column.header().innerText;
+        let column = this;
+        const title = column.header().innerText;
 
-        var select = $('<select class="form-control" style="width: 100%; padding: 0; height: 20px;"><option value="">'+title+'</option></select>')
+        let select = $('<select class="form-control" style="width: 100%; padding: 0; height: 20px;"><option value="">'+title+'</option></select>')
             .appendTo( $(column.header()).empty() )
             .on( 'change', function () {
-                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                const val = $.fn.dataTable.util.escapeRegex($(this).val());
                 //column.search( val ? '^'+val+'$' : '', true, false ).draw();
                 column.search( val ? ''+val+'' : '', true, false ).draw();
             }).on( 'click', function( event ) {
@@ -15,7 +15,7 @@ function dataTableColumnFilter( api ) {
 
         column.data().unique().sort().each( function ( d, j ) {
             if(d){
-                var val = $.fn.dataTable.util.escapeRegex(d);
+                const val = $.fn.dataTable.util.escapeRegex(d);
                 // if (column.search() === "^" + val + "$") {
                 if (column.search() === "" + val + "") {
                     select.append(
@@ -26,14 +26,11 @@ function dataTableColumnFilter( api ) {
                 }
             }
         });
-
-
     });
 }
 
-
 $(document).on( 'init.dt', function ( e, settings ) {
-    var api = new $.fn.dataTable.Api( settings );
+    let api = new $.fn.dataTable.Api( settings );
 
     if ( api.init().dataTableColumnFilter || $.fn.dataTable.defaults.dataTableColumnFilter ){
         new dataTableColumnFilter( api );
